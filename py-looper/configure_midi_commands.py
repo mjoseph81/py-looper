7469b67dacc1bc0be0bd952d8ec2db3c2f32f84b
mjoseph81 <mjoseph81@gmail.com>
2023-08-20 07:32:06 -0400

    Update configure_midi_commands.py

--- py-looper/configure_midi_commands.py
+++ py-looper/configure_midi_commands.py
@@ -1,14 +1,14 @@
 parameters = []
 
-parameters.append(input('Enter MIDI Control Number for MODE): '))
-parameters.append(input('Enter MIDI Control Number for RESET): '))
-parameters.append(input('Enter MIDI Control Number for CLEAR): '))
+parameters.append(input('Enter MIDI Control Number for MODE: '))
+parameters.append(input('Enter MIDI Control Number for RESET: '))
+parameters.append(input('Enter MIDI Control Number for CLEAR: '))
 parameters.append(input('Enter MIDI Control Number for REC/PLAY): '))
-parameters.append(input('Enter MIDI Control Number for STOP): '))
-parameters.append(input('Enter MIDI Control Number for TRACK 1): '))
-parameters.append(input('Enter MIDI Control Number for TRACK 2): '))
-parameters.append(input('Enter MIDI Control Number for TRACK 3): '))
-parameters.append(input('Enter MIDI Control Number for TRACK 4): '))
+parameters.append(input('Enter MIDI Control Number for STOP: '))
+parameters.append(input('Enter MIDI Control Number for TRACK 1: '))
+parameters.append(input('Enter MIDI Control Number for TRACK 2: '))
+parameters.append(input('Enter MIDI Control Number for TRACK 3: '))
+parameters.append(input('Enter MIDI Control Number for TRACK 4: '))
 parameters.append(input('Enter COM Port for MIDI device (Ex. /dev/ttyACM0) : '))
 
 print(parameters)
