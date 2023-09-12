9ef0870408dc75e32a43ac5bf3701eb5591b6d7c
matt <mjoseph81@gmail.com>
2023-09-11 20:16:04 -0400

    added support to plot both left and right channels for each track

--- py-looper/py-looper.py
+++ py-looper/py-looper.py
@@ -1042,36 +1042,50 @@ class Track(tk.Tk):
         
         
         #get just the left channel
-        track1_l = track1[::2]
-        track2_l = track2[::2]
-        track3_l = track3[::2]
-        track4_l = track4[::2]
+        track1_l = track1[0::2]
+        track2_l = track2[0::2]
+        track3_l = track3[0::2]
+        track4_l = track4[0::2]
+        
+        #get just the right channel
+        track1_r = track1[1::2]
+        track2_r = track2[1::2]
+        track3_r = track3[1::2]
+        track4_r = track4[1::2]
 
         times = np.linspace(0, t_audio, num=n_samples)
         
         fig, axs = plt.subplots(3,2)
         fig.suptitle('Audio Waveforms')
         axs[0,0].plot(times, track1_l, 'tab:blue')
+        axs[0,0].plot(times, track1_r, 'tab:red')
         axs[0,0].set_title('Track 1')
         #axs[0,0].set_ylim([-30000, 30000])
        
-        axs[0,1].plot(times, track2_l, 'tab:red')
+        axs[0,1].plot(times, track2_l, 'tab:blue')
+        axs[0,1].plot(times, track2_r, 'tab:red')
         axs[0,1].set_title('Track 2')
         #axs[0,1].set_ylim([-30000, 30000])
         
         axs[1,0].plot(times, track3_l, 'tab:green')
+        axs[1,0].plot(times, track3_r, 'tab:orange')
         axs[1,0].set_title('Track 3')
         #axs[1,0].set_ylim([-30000, 30000])
         
-        axs[1,1].plot(times, track4_l, 'tab:orange')
+        axs[1,1].plot(times, track4_l, 'tab:green')
+        axs[1,1].plot(times, track4_r, 'tab:orange')
         axs[1,1].set_title('Track 4')
         #axs[1,1].set_ylim([-30000, 30000])
         
         axs[2,0].plot(times, track1_l, 'tab:blue')
+        axs[2,0].plot(times, track1_r, 'tab:blue')
         axs[2,0].plot(times, track2_l, 'tab:red')
+        axs[2,0].plot(times, track2_r, 'tab:red')
         axs[2,0].plot(times, track3_l, 'tab:green')
+        axs[2,0].plot(times, track3_r, 'tab:green')
         axs[2,0].plot(times, track4_l, 'tab:orange')
-        #axs[2,0].set_ylim([-30000, 30000])
+        axs[2,0].plot(times, track4_r, 'tab:orange')
+        #axs[1,1].set_ylim([-30000, 30000])
         figManager = plt.get_current_fig_manager()
         figManager.window.showMaximized()
         fig.tight_layout()
